7519fb35e9791af9e8e90ca1bb84dc3f6e357cb0
mohammodrubel <md.rubel007@hotmail.com>
2025-06-15 14:11:15 +0600

    mejor vendor added slidder

--- src/components/MajorTopVendor.jsx
+++ src/components/MajorTopVendor.jsx
@@ -1,20 +1,34 @@
-import Image from "next/image"
-import majorImage from '../assets/f.png'
+import Image from "next/image";
+import majorImage from "../assets/f.png";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from "@/components/ui/carousel";
 
-export default function MajorTopVendor() {
+function MajorTopVendor() {
   return (
-    <div className="mt-2 mb-4 rounded-lg p-2 shadow-sm">
-      <div className="rounded-lg p-2 flex justify-center">
-        <div className="relative w-full h-[600px]">
-          <Image
-            src={majorImage}
-            alt="Vendor"
-            fill
-            className="object-contain"
-          />
-        </div>
-
-      </div>
+    <div>
+      <Carousel>
+        <CarouselContent className="w-full">
+          {[1, 2, 3].map((_, index) => (
+            <CarouselItem key={index} className="relative h-[600px]">
+              <Image
+                src={majorImage}
+                alt={`slider image ${index + 1}`}
+                fill
+                className="object-contain"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
-  )
+  );
 }
+
+export default MajorTopVendor;
