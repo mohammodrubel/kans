5b63d304603cff511b6ff2db73bdd9ecca300318
mohammodrubel <md.rubel007@hotmail.com>
2025-08-09 12:14:34 +0600

    dynamic customar

--- src/components/Customar/Customar.jsx
+++ src/components/Customar/Customar.jsx
@@ -1,59 +1,62 @@
+"use client"
+
 import Image from 'next/image'
 import Marquee from 'react-fast-marquee'
-import customer1 from '../../assets/customarlogo/img1.png'
-import customer2 from '../../assets/customarlogo/img2.png'
-import customer3 from '../../assets/customarlogo/img3.png'
-import customer4 from '../../assets/customarlogo/img4.png'
-import customer5 from '../../assets/customarlogo/img5.png'
-import customer6 from '../../assets/customarlogo/img6.png'
-import customer7 from '../../assets/customarlogo/img7.png'
-import customer8 from '../../assets/customarlogo/img8.png'
-import customer9 from '../../assets/customarlogo/img9.png'
+import { useState, useEffect } from 'react'
+import { getAllCustomers } from '@/app/api/customers'
 
 function Customar() {
-    const clientLogos = [
-        { name: "Client 1", logo: customer1 },
-        { name: "Client 2", logo: customer2 },
-        { name: "Client 3", logo: customer3 },
-        { name: "Client 4", logo: customer4 },
-        { name: "Client 5", logo: customer5 },
-        { name: "Client 6", logo: customer6 },
-        { name: "Client 7", logo: customer7 },
-        { name: "Client 8", logo: customer8 },
-        { name: "Client 9", logo: customer9 },
-    ]
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchCustomers = async () => {
+            try {
+                const res = await getAllCustomers()
+                setData(res?.data)
+            } catch (error) {
+                console.error("Error fetching customers:", error)
+            }
+        }
+        fetchCustomers()
+    }, [])
 
     return (
         <section className="bg-gradient-to-br from-gray-50 to-gray-100 py-20">
-            <div className="container mx-auto  sm:px-6 lg:px-8">
+            <div className="container mx-auto sm:px-6 lg:px-8">
                 <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-900 leading-tight mb-6">
                     Meet Some of Our Clients
                 </h1>
 
                 {/* Marquee Left to Right */}
                 <Marquee direction="right" className="flex items-center gap-12 mb-10" speed={40} pauseOnHover>
-                    {clientLogos.map((item, index) => (
-                        <div key={index} className="bg-white p-4 m-4 rounded shadow-md">
-                            <Image
-                                src={item.logo}
-                                width={100}
-                                height={100}
-                                alt={item.name}
-                            />
+                    {data.map((customer) => (
+                        <div key={customer.id} className="bg-white p-4 m-4 rounded shadow-md">
+                            {customer?.photo?.url && (
+                                <Image
+                                    src={customer.photo.url}
+                                    width={100}
+                                    height={100}
+                                    alt={customer.first_name || "Customer"}
+                                    unoptimized // because it's an external image
+                                />
+                            )}
                         </div>
                     ))}
                 </Marquee>
 
                 {/* Marquee Right to Left */}
                 <Marquee direction="left" className="flex items-center gap-12" speed={40} pauseOnHover>
-                    {clientLogos.map((item, index) => (
-                        <div key={index} className="bg-white p-4 m-4 rounded shadow-md">
-                            <Image
-                                src={item.logo}
-                                width={100}
-                                height={100}
-                                alt={item.name}
-                            />
+                    {data.map((customer) => (
+                        <div key={customer.id} className="bg-white p-4 m-4 rounded shadow-md">
+                            {customer?.photo?.url && (
+                                <Image
+                                    src={customer.photo.url}
+                                    width={100}
+                                    height={100}
+                                    alt={customer.first_name || "Customer"}
+                                    unoptimized
+                                />
+                            )}
                         </div>
                     ))}
                 </Marquee>
